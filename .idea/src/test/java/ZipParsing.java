c2e3aaa41c43d4128b64d50ab8a9e1a38e461230
Anastasia Lyuleva <nesti-1998@mail.ru>
2025-02-03 00:16:06 +0300

    added files

--- .idea/src/test/java/ZipParsing.java
+++ .idea/src/test/java/ZipParsing.java
@@ -1,13 +1,12 @@
-import java.io.File;
-import java.io.InputStream;
-import java.util.Enumeration;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
-
 import com.codeborne.xlstest.XLS;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class ZipParsing {
     private ClassLoader cl = ZipParsing.class.getClassLoader();
@@ -18,8 +17,9 @@ public class ZipParsing {
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
                 if (entry.getName().contains(".xlsx")) {
-                    XLS xls = new XLS(zis);
-
+                    XLS xlsx = new XLS(zis);
+                    String value = xlsx.excel.getSheetAt(0).getRow(1).getCell(3).getStringCellValue();
+                    Assertions.assertTrue(value.contains("Лекция"));
                 }
             }
         }
