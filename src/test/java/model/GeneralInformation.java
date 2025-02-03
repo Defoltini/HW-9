c7ed00e1dab7e55009dadf48c6a433577fea5e9d
Anastasia Lyuleva <nesti-1998@mail.ru>
2025-02-03 23:05:11 +0300

    code alignment

--- src/test/java/model/GeneralInformation.java
+++ src/test/java/model/GeneralInformation.java
@@ -4,51 +4,51 @@ import java.util.List;
 
 public class GeneralInformation {
 
-        private String title;
-        private String author;
-        private String summary;
-        private List<String> genres;
-        private Integer publication_year;
+    private String title;
+    private String author;
+    private String summary;
+    private List<String> genres;
+    private Integer publication_year;
 
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public String getAuthor() {
-            return author;
-        }
-
-        public void setAuthor(String author) {
-            this.author = author;
-        }
-
-        public String getSummary() {
-            return summary;
-        }
-
-        public void setSummary(String summary) {
-            this.summary = summary;
-        }
-
-        public List<String> getGenres() {
-            return genres;
-        }
-
-        public void setGenres(List<String> genres) {
-            this.genres = genres;
-        }
-
-        public Integer getPublication_year() {
-            return publication_year;
-        }
-
-        public void setPublication_year(Integer publication_year) {
-            this.publication_year = publication_year;
-        }
+    public String getTitle() {
+        return title;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public Integer getPublication_year() {
+        return publication_year;
+    }
+
+    public void setPublication_year(Integer publication_year) {
+        this.publication_year = publication_year;
+    }
+}
 
 
